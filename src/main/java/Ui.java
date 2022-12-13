70c5e501af04a782b9f7548276c22d48f32af127
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-12-13 21:28:44 +0100

    Arrays

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Ui {
@@ -5,6 +6,9 @@ public class Ui {
     Person person1 = new Person("Harry", "Potter", 100);
     Person person2 = new Person("Ron", "Weasley", 231);
     Person person3 = new Person("Hermione", "Granger", 420);
+    Person person4 = new Person("Ginny", "Weasley", 1000);
+    Person person5 = new Person("Draco", "Malfoy", 215);
+    Person person6 = new Person("Neville", "Longbottom", 7);
     public void start(){
         System.out.println(person1.toString());
         System.out.println(person2.toString());
@@ -13,7 +17,7 @@ public class Ui {
     }
 
     public void arrays(){
-        Person personer [] = {person1, person2, person3};
+        Person personer [] = {person4, person2, person3, person1};
         String personNavne [] = {person1.getFirstName(), person2.getFirstName(), person3.getFirstName()};
         System.out.println("--------------------------------------");
         System.out.println(Arrays.toString(personer));
@@ -23,5 +27,8 @@ public class Ui {
         for (Person personere : personer){
             System.out.println(personere);
         }
+
+
+
     }
 }
