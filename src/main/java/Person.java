32d9d874678e38c5f699ecfd8ace9a83a784121b
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-12-13 18:16:03 +0100

    del person done

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,12 +7,45 @@ public class Person {
     public Person(){
     }
 
-    public Person(String firstName, String lastName, int points){
+    public Person(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
         this.points = 0;
     }
+    public Person(String firstName, String lastName, int points){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.points = points;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int addPoints(int points){
+        points += getPoints();
+        return points;
+    }
     @Override
     public String toString() {
         return firstName + " " + lastName + " har " + points + " point";
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,7 +1,11 @@
 public class Ui {
     Person person = new Person();
     public void start(){
-        Person person1 = new Person("Harry", "Potter", 0);
+        Person person1 = new Person("Harry", "Potter", 100);
+        Person person2 = new Person("Ron", "Weasley", 231);
+        Person person3 = new Person("Hermione", "Granger", 420);
         System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
     }
 }
