813e50a23279347c2d0ff0a35c90a0a7580e35b7
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-12-13 21:12:08 +0100

    enhanced for loop

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,11 +1,27 @@
+import java.util.Arrays;
+
 public class Ui {
     Person person = new Person();
+    Person person1 = new Person("Harry", "Potter", 100);
+    Person person2 = new Person("Ron", "Weasley", 231);
+    Person person3 = new Person("Hermione", "Granger", 420);
     public void start(){
-        Person person1 = new Person("Harry", "Potter", 100);
-        Person person2 = new Person("Ron", "Weasley", 231);
-        Person person3 = new Person("Hermione", "Granger", 420);
         System.out.println(person1.toString());
         System.out.println(person2.toString());
         System.out.println(person3.toString());
+        arrays();
+    }
+
+    public void arrays(){
+        Person personer [] = {person1, person2, person3};
+        String personNavne [] = {person1.getFirstName(), person2.getFirstName(), person3.getFirstName()};
+        System.out.println("--------------------------------------");
+        System.out.println(Arrays.toString(personer));
+        System.out.println("--------------------------------------");
+        System.out.println(Arrays.toString(personNavne));
+        System.out.println("--------------------------------------");
+        for (Person personere : personer){
+            System.out.println(personere);
+        }
     }
 }
